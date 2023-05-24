505abaf527dca377d4e1b556da65acfc06ca7938
wbasp <maad7445@gmail.com>
2023-05-24 10:37:33 +0600

    js added with comment code

---
+++ program.js
@@ -0,0 +1,18 @@
+const bod = document.getElementsByTagName('body')[0];
+const but = document.getElementById('inner-but');
+// bod.style.backgroundColor = 'black';
+// function changeColor(){
+//     if(bod.style.backgroundColor == 'black'){
+//         bod.style.backgroundColor = 'white';
+//         bod.style.color = 'black';
+//         but.style.backgroundColor = 'black';
+//         but.style.top = '50px';
+//     }
+//     else{
+//         bod.style.backgroundColor = 'black';
+//         bod.style.color = 'white';
+//         but.style.backgroundColor = 'white';
+//         but.style.top = '0px';
+//     }
+// }
+// but.addEventListener('click', changeColor);
